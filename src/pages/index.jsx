9683206eb8829b2e9954f1f653f068be8aa9e702
digitechlifestyle <131618129+digitechlifestyle@users.noreply.github.com>
2026-05-25 17:59:17 +0100

    Set React Router basename for GitHub Pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -61,65 +61,35 @@ import NewsletterCampaigns from "./NewsletterCampaigns";
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 const PAGES = {
-    
-    Home: Home,
-    
-    Browse: Browse,
-    
-    Category: Category,
-    
-    Featured: Featured,
-    
-    Admin: Admin,
-    
-    Pricing: Pricing,
-    
-    AddListing: AddListing,
-    
-    Project: Project,
-    
-    Dashboard: Dashboard,
-    
-    Blog: Blog,
-    
-    BlogPost: BlogPost,
-    
-    About: About,
-    
-    EditorialStandards: EditorialStandards,
-    
-    Contact: Contact,
-    
-    BrandAssets: BrandAssets,
-    
-    AffiliateProgram: AffiliateProgram,
-    
-    EditBlogPost: EditBlogPost,
-    
-    CreateBlogPost: CreateBlogPost,
-    
-    AdManagement: AdManagement,
-    
-    SiteHealthCheck: SiteHealthCheck,
-    
-    BlogContentStrategy: BlogContentStrategy,
-    
-    Settings: Settings,
-    
-    PaymentIntegrations: PaymentIntegrations,
-    
-    AffiliateTutorial: AffiliateTutorial,
-    
-    Advertise: Advertise,
-    
-    Rewards: Rewards,
-    
-    Compare: Compare,
-    
-    Learn: Learn,
-    
-    NewsletterCampaigns: NewsletterCampaigns,
-    
+    Home,
+    Browse,
+    Category,
+    Featured,
+    Admin,
+    Pricing,
+    AddListing,
+    Project,
+    Dashboard,
+    Blog,
+    BlogPost,
+    About,
+    EditorialStandards,
+    Contact,
+    BrandAssets,
+    AffiliateProgram,
+    EditBlogPost,
+    CreateBlogPost,
+    AdManagement,
+    SiteHealthCheck,
+    BlogContentStrategy,
+    Settings,
+    PaymentIntegrations,
+    AffiliateTutorial,
+    Advertise,
+    Rewards,
+    Compare,
+    Learn,
+    NewsletterCampaigns,
 }
 
 function _getCurrentPage(url) {
@@ -135,76 +105,44 @@ function _getCurrentPage(url) {
     return pageName || Object.keys(PAGES)[0];
 }
 
-// Create a wrapper component that uses useLocation inside the Router context
 function PagesContent() {
     const location = useLocation();
     const currentPage = _getCurrentPage(location.pathname);
     
     return (
         <Layout currentPageName={currentPage}>
-            <Routes>            
-                
-                    <Route path="/" element={<Home />} />
-                
-                
+            <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/Home" element={<Home />} />
-                
                 <Route path="/Browse" element={<Browse />} />
-                
                 <Route path="/Category" element={<Category />} />
-                
                 <Route path="/Featured" element={<Featured />} />
-                
                 <Route path="/Admin" element={<Admin />} />
-                
                 <Route path="/Pricing" element={<Pricing />} />
-                
                 <Route path="/AddListing" element={<AddListing />} />
-                
                 <Route path="/Project" element={<Project />} />
-                
                 <Route path="/Dashboard" element={<Dashboard />} />
-                
                 <Route path="/Blog" element={<Blog />} />
-                
                 <Route path="/BlogPost" element={<BlogPost />} />
-                
                 <Route path="/About" element={<About />} />
-                
                 <Route path="/EditorialStandards" element={<EditorialStandards />} />
-                
                 <Route path="/Contact" element={<Contact />} />
-                
                 <Route path="/BrandAssets" element={<BrandAssets />} />
-                
                 <Route path="/AffiliateProgram" element={<AffiliateProgram />} />
-                
                 <Route path="/EditBlogPost" element={<EditBlogPost />} />
-                
                 <Route path="/CreateBlogPost" element={<CreateBlogPost />} />
-                
                 <Route path="/AdManagement" element={<AdManagement />} />
-                
                 <Route path="/SiteHealthCheck" element={<SiteHealthCheck />} />
-                
                 <Route path="/BlogContentStrategy" element={<BlogContentStrategy />} />
-                
                 <Route path="/Settings" element={<Settings />} />
-                
                 <Route path="/PaymentIntegrations" element={<PaymentIntegrations />} />
-                
                 <Route path="/AffiliateTutorial" element={<AffiliateTutorial />} />
-                
                 <Route path="/Advertise" element={<Advertise />} />
-                
                 <Route path="/Rewards" element={<Rewards />} />
-                
                 <Route path="/Compare" element={<Compare />} />
-                
                 <Route path="/Learn" element={<Learn />} />
-                
                 <Route path="/NewsletterCampaigns" element={<NewsletterCampaigns />} />
-                
+                <Route path="*" element={<Home />} />
             </Routes>
         </Layout>
     );
@@ -212,7 +150,7 @@ function PagesContent() {
 
 export default function Pages() {
     return (
-        <Router>
+        <Router basename={import.meta.env.BASE_URL}>
             <PagesContent />
         </Router>
     );
